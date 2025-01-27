05145359c51ed53fdd57c111821bffaac6d61d90
Thiago Bezerra de Carvalho <thiagaonaruto@gmail.com>
2025-01-27 13:55:53 -0300

    Reaprendendo

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// ta bem simples, eu sei, mas é mais para relembrar.
+
+Console.WriteLine("♦ Sequência de de números ♦\n");
+Console.WriteLine("aperte uma tecla...\n");
+Console.ReadKey();
+Console.Clear();
+
+Console.WriteLine("1\n");
+Console.WriteLine("aperte uma tecla...\n");
+Console.ReadKey();
+Console.Clear();
+
+Console.WriteLine("2\n");
+Console.WriteLine("aperte uma tecla...\n");
+Console.ReadKey();
+Console.Clear();
+
+Console.WriteLine("3\n");
+Console.WriteLine("aperte uma tecla para finalizar...\n");
+Console.ReadKey();
+Console.Clear();
